28e1d63d7734716466fe8b7037800d7bfa3eaca6
Chris Williamson <chriswill0629@gmail.com>
2022-10-15 13:23:31 -0400

    finished search function code reduction

--- src/data/dataContext.jsx
+++ src/data/dataContext.jsx
@@ -37,18 +37,42 @@ export const DataProvider = ({ children }) => {
 		{ value: "MYCRUISEVACATION.COM", label: "MYCRUISEVACATION.COM" },
 		{ value: "UNKNOWN", label: "UNKNOWN" },
 	];
+	const [programValue, setProgramValue] = useState("");
+
 	const handleProgramChange = (value) => {
+		if (value === null) {
+			setProgramValue("");
+		} else if (value.value === "UNKNOWN") {
+			setProgramValue("");
+		} else {
+			setProgramValue(value.value);
+		}
 		setProgramSelection(value);
 	};
 
 	const countryOptions = useMemo(() => countryList().getData(), []);
+
+	const [country, setCountry] = useState("");
 	const [countrySel, setCountrySel] = useState(null);
+
+	const [state, setState] = useState("");
 	const [stateSel, setStateSel] = useState(null);
 
 	const handleCountryChange = (value) => {
+		//console.log(value.value);
+		if (value === null) {
+			setCountry("");
+		} else {
+			setCountry(value.value);
+		}
 		setCountrySel(value);
 	};
 	const handleStateChange = (value) => {
+		if (value === null) {
+			setState("");
+		} else {
+			setState(value.value);
+		}
 		setStateSel(value);
 	};
 	const searchReset = () => {
@@ -60,39 +84,38 @@ export const DataProvider = ({ children }) => {
 		setCcNumber("");
 		setSearchResultList([]);
 		setnewCustBtn(false);
-		setProgramSelection(null);
-		setCountrySel(null);
-		setStateSel(null);
+		handleProgramChange(null);
+		handleStateChange(null);
+		handleCountryChange(null);
 	};
-	//1. Create generic filter function for your object schema
-	const eqStrictFirstName = (name) => filter((x) => x.firstName === name);
-	const hasPhone = (num) => filter((x) => x.phone.includes(num));
 
-	//2. Make a generic function to combine those functions
-	//according to your business logic
-	const and = curry((predicates, item) => {
-		return predicates.reduce(
-			(hasPassedSoFar, predicate) => hasPassedSoFar && predicate(item),
-			true
-		);
-	});
+	const filterParams = {
+		program: programValue,
+		firstName: fname,
+		lastName: lname,
+		phone: phone,
+		email: email,
+		country: country,
+		state: state,
+	};
+
+	const search = (objArray, filterParams) => {
+		console.log(filterParams);
+		return objArray.filter((obj) => {
+			return Object.keys(filterParams).every((key) => {
+				return obj[key]
+					.toLowerCase()
+					.includes(filterParams[key].toLowerCase());
+			});
+		});
+	};
+
 	const searchCustomer = () => {
-		console.log("results ", searchResultList);
 		togglenewCustBtn();
 		setSearchResultList([]);
 
-		const fL = fname.length;
-		const LL = lname.length;
-		const fnL = fname.toLowerCase();
-		const lnL = lname.toLowerCase();
-		const emL = email.toLowerCase();
 		let tempArray = [];
 
-		if (fname !== "" || lname !== "") {
-			if (fL < 2 || LL < 2) {
-				alert("last name and first name must be at least 2 letters!");
-			}
-		}
 		if (custNumber !== "") {
 			customerData.map((item, i) => {
 				if (item.id === custNumber) {
@@ -100,324 +123,19 @@ export const DataProvider = ({ children }) => {
 				}
 			});
 		} else if (programSelection !== null) {
-			tempArray = and(
-				[eqStrictFirstName(fnL), hasPhone(phone)],
-				customerData
-			);
-			console.log(
-				and([eqStrictFirstName(fnL), hasPhone(phone)], customerData)
-			);
-			console.log("tempArray ", tempArray);
-			/*customerData.map((item, i) => {
-				const ifn = item.firstName.toLowerCase();
-				const iln = item.lastName.toLowerCase();
-				const iem = item.email.toLowerCase();
-
-				if (programSelection.value === "UNKNOWN") {
-					let array1 = customerData.map((item, i) => {
-						if (item.phone === phone) {
-							return item.id;
-						} else {
-							return null;
-						}
-					});
-					tempArray = array1;
-				} else if (item.program === programSelection.value) {
-					if (item.id === custNumber) return tempArray.push(item.id);
-				}
-			});*/
-		}
-		/*
-		if (programSelection !== null) {
-			if (custNumber !== "") {
-				customerData.map((item, i) => {
-					if (programSelection.value === "UNKNOWN") {
-						if (item.id === custNumber) {
-							tempArray.push(item.id);
-						}
-					} else if (
-						item.program === programSelection.value &&
-						item.id === custNumber
-					) {
-						tempArray.push(item.id);
-					}
-				});
-
-				//*************Search by country, state, or customer #***************
-			} else if (countrySel !== null && stateSel === null) {
-				if (phone !== "") {
-					customerData.map((item, i) => {
-						if (programSelection.value === "UNKNOWN") {
-							if (
-								item.phone === phone &&
-								item.country === countrySel.value
-							) {
-								tempArray.push(item.id);
-							}
-						} else if (
-							item.program === programSelection.value &&
-							item.phone === phone &&
-							item.country === countrySel.value
-						) {
-							tempArray.push(item.id);
-						}
-					});
-				} else if (lname !== "" && fname !== "") {
-					customerData.map((item, i) => {
-						const ifn = item.firstName.toLowerCase();
-						const iln = item.lastName.toLowerCase();
-						if (programSelection.value === "UNKNOWN") {
-							if (
-								ifn.includes(fnameLower) &&
-								iln.includes(lnameLower) &&
-								item.country === countrySel.value
-							) {
-								tempArray.push(item.id);
-							}
-						} else if (
-							item.program === programSelection.value &&
-							ifn.includes(fnameLower) &&
-							iln.includes(lnameLower) &&
-							item.country === countrySel.value
-						) {
-							tempArray.push(item.id);
-						}
-					});
-				} else {
-					alert(
-						"Must have phone OR first and last name to search by country"
-					);
-				}
-			} else if (countrySel !== null && stateSel !== null) {
-				if (phone !== "") {
-					customerData.map((item, i) => {
-						if (programSelection.value === "UNKNOWN") {
-							if (
-								item.phone === phone &&
-								item.country === countrySel.value &&
-								item.state === stateSel.value
-							) {
-								tempArray.push(item.id);
-							}
-						} else if (
-							item.program === programSelection.value &&
-							item.phone === phone &&
-							item.country === countrySel.value &&
-							item.state === stateSel.value
-						) {
-							tempArray.push(item.id);
-						}
-					});
-				} else if (lname !== "" && fname !== "") {
-					customerData.map((item, i) => {
-						const ifn = item.firstName.toLowerCase();
-						const iln = item.lastName.toLowerCase();
-						if (programSelection.value === "UNKNOWN") {
-							if (
-								ifn.includes(fnameLower) &&
-								iln.includes(lnameLower) &&
-								item.country === countrySel.value &&
-								item.state === stateSel.value
-							) {
-								tempArray.push(item.id);
-							}
-						} else if (
-							item.program === programSelection.value &&
-							ifn.includes(fnameLower) &&
-							iln.includes(lnameLower) &&
-							item.country === countrySel.value &&
-							item.state === stateSel.value
-						) {
-							tempArray.push(item.id);
-						}
-					});
-				} else {
-					alert(
-						"Must have phone OR first and last name to search by country"
-					);
-				}
-			} else if (countrySel === null && stateSel !== null) {
-				if (phone !== "") {
-					customerData.map((item, i) => {
-						if (programSelection.value === "UNKNOWN") {
-							if (
-								item.phone === phone &&
-								item.state === stateSel.value
-							) {
-								tempArray.push(item.id);
-							}
-						} else if (
-							item.program === programSelection.value &&
-							item.phone === phone &&
-							item.state === stateSel.value
-						) {
-							tempArray.push(item.id);
-						}
-					});
-				} else if (lname !== "" && fname !== "") {
-					customerData.map((item, i) => {
-						const ifn = item.firstName.toLowerCase();
-						const iln = item.lastName.toLowerCase();
-						if (programSelection.value === "UNKNOWN") {
-							if (
-								ifn.includes(fnameLower) &&
-								iln.includes(lnameLower) &&
-								item.state === stateSel.value
-							) {
-								tempArray.push(item.id);
-							}
-						} else if (
-							item.program === programSelection.value &&
-							ifn.includes(fnameLower) &&
-							iln.includes(lnameLower) &&
-							item.state === stateSel.value
-						) {
-							tempArray.push(item.id);
-						}
-					});
-				} else {
-					alert(
-						"Must have phone OR first and last name to search by country"
-					);
-				}
-				//*************Search by Phone with email and name***************
-			} else if (phone !== "") {
-				if (email === "" && fname === "" && lname === "") {
-					customerData.map((item, i) => {
-						if (programSelection.value === "UNKNOWN") {
-							if (item.phone === phone) {
-								tempArray.push(item.id);
-							}
-						} else if (
-							item.program === programSelection.value &&
-							item.phone === phone
-						) {
-							tempArray.push(item.id);
-						}
-					});
-				} else if (email !== "" && fname === "" && lname === "") {
-					customerData.map((item, i) => {
-						const iem = item.email.toLowerCase();
-						if (programSelection.value === "UNKNOWN") {
-							if (item.phone === phone && iem === emLower) {
-								tempArray.push(item.id);
-							}
-						} else if (
-							item.program === programSelection.value &&
-							item.phone === phone &&
-							iem === emLower
-						) {
-							tempArray.push(item.id);
-						}
-					});
-				} else if (fname !== "" && lname !== "") {
-					if (fL < 2 || LL < 2) {
-						alert(
-							"last name and first name must be at least 2 letters!"
-						);
-					}
-					customerData.map((item, i) => {
-						const ifn = item.firstName.toLowerCase();
-						const iln = item.lastName.toLowerCase();
-
-						if (programSelection.value === "UNKNOWN") {
-							if (
-								item.phone === phone &&
-								ifn.includes(fnameLower) &&
-								iln.includes(lnameLower)
-							) {
-								tempArray.push(item.id);
-							}
-						} else if (
-							item.program === programSelection.value &&
-							item.phone === phone &&
-							ifn.includes(fnameLower) &&
-							iln.includes(lnameLower)
-						) {
-							tempArray.push(item.id);
-						}
-					});
-				}
-				//*************Search by Email with name*****************
-			} else if (email !== "") {
-				if (fname === "" && lname === "") {
-					customerData.map((item, i) => {
-						if (programSelection.value === "UNKNOWN") {
-							if (emLower === email.toLowerCase()) {
-								tempArray.push(item.id);
-							}
-						} else if (
-							item.program === programSelection.value &&
-							item.email === email
-						) {
-							tempArray.push(item.id);
-						}
-					});
-				} else if (fname !== "" && lname !== "") {
-					if (fL < 2 || LL < 2) {
-						alert(
-							"last name and first name must be at least 2 letters!"
-						);
-					}
-					customerData.map((item, i) => {
-						const ifn = item.firstName.toLowerCase();
-						const iln = item.lastName.toLowerCase();
-						const iem = item.email.toLowerCase();
-
-						if (programSelection.value === "UNKNOWN") {
-							if (
-								emLower === iem &&
-								ifn.includes(fnameLower) &&
-								iln.includes(lnameLower)
-							) {
-								tempArray.push(item.id);
-							}
-						} else if (
-							item.program === programSelection.value &&
-							emLower === iem &&
-							ifn.includes(fnameLower) &&
-							iln.includes(lnameLower)
-						) {
-							tempArray.push(item.id);
-						}
-					});
-				}
-				//************ Search by name only***************
-			} else if (lname !== "" || fname !== "") {
-				if (fL >= 2 && LL >= 2) {
-					customerData.map((item, i) => {
-						const ifn = item.firstName.toLowerCase();
-						const iln = item.lastName.toLowerCase();
-
-						if (programSelection.value === "UNKNOWN") {
-							//****************
-							if (
-								ifn.includes(fnameLower) &&
-								iln.includes(lnameLower)
-							) {
-								tempArray.push(item.id);
-							}
-						} else if (
-							item.program === programSelection.value &&
-							ifn.includes(fnameLower) &&
-							iln.includes(lnameLower)
-						) {
-							tempArray.push(item.id);
-						}
-					});
-				} else {
+			if (fname !== "" || lname !== "") {
+				if (fname.length < 2 || lname.length < 2) {
 					alert(
 						"last name and first name must be at least 2 letters!"
 					);
+				} else {
+					tempArray = search(customerData, filterParams);
 				}
-			} else {
-				alert("Search field empty!");
 			}
-
-			//**************Program Selection is needed for search***************
 		} else {
-			alert("Please select a program!");
-		}*/
+			alert("Program must be selected!");
+		}
+
 		setSearchResultList(tempArray);
 	};
 
